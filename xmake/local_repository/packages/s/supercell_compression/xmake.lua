10614494fd33b0059904f1895ff0e3124cc095d8
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2024-09-18 19:13:03 +0100

    remove static

--- xmake/local_repository/packages/s/supercell_compression/xmake.lua
+++ xmake/local_repository/packages/s/supercell_compression/xmake.lua
@@ -4,6 +4,7 @@ package("supercell_compression")
     set_license("MIT")
 
     add_urls("https://github.com/Chi-EEE/SupercellCompression.git")
+    add_versions("2024.09.18", "6c670d6515bc95e51e56c18e16ca37f01f903976")
     add_versions("2024.06.21", "9bea65e9308f3694788001e59209325a1cec7982")
 
     add_deps("lzham_codec", "lzma", "zstd")
--- xmake/local_repository/packages/s/supercell_flash/xmake.lua
+++ xmake/local_repository/packages/s/supercell_flash/xmake.lua
@@ -4,6 +4,7 @@ package("supercell_flash")
     set_license("MIT")
 
     add_urls("https://github.com/Chi-EEE/SupercellFlash.git")
+    add_versions("2024.09.18", "d54c625e266f3467423aa1bc80d64d2a9ef75b25")
     add_versions("2024.09.15", "d7879b6b550c94c56b5240190bbd67d9d55a9774")
     add_versions("2024.06.21", "f824f7f84b892a14f5886515ad16ade45c034f48")
 
